d6f4d22f0d06295a5144b1b8537464f36f00c010
Erik Bolumburu <108585396+erikbolumburu11@users.noreply.github.com>
2024-04-17 23:28:33 +0100

    Cleaned Stuff Up

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum State
+public enum State
 {
-    PLAYER_INITIAL_UNIT_PLACEMENT,
+    INITIAL_UNIT_PLACEMENT,
+    BEFORE_AI_TURN,
+    AI_TURN,
+    AFTER_AI_TURN,
     BEFORE_PLAYER_TURN,
     PLAYER_TURN,
     AFTER_PLAYER_TURN,
@@ -15,10 +18,13 @@ public class GameManager : MonoBehaviour
 {
     WorldMap map;
 
-    UIManager uiManager;
-    UnitMovementUI unitMovementUI;
+    public UIManager uiManager;
+    public UnitMovementUI unitMovementUI;
 
     [SerializeField] PlayerDataManager playerDataManager;
+
+    public bool initialUnitPlacementComplete = false;
+    public bool playerTurnComplete = false;
 
     public List<Player> players;
     public Player currentTurnsPlayer;
@@ -27,7 +33,7 @@ public class GameManager : MonoBehaviour
     public Territory currentlyDefendingTerritory;
     public Territory currentlyAttackingTerritory;
 
-    State state = State.PLAYER_INITIAL_UNIT_PLACEMENT;
+    State state = State.INITIAL_UNIT_PLACEMENT;
 
     public bool unitAmountToMoveConfirmed = false;
 
@@ -42,16 +48,16 @@ public class GameManager : MonoBehaviour
         ChangeState(state);
     }
 
-    void ChangeState(State newState)
+    public void ChangeState(State newState)
     {
         switch (newState)
         {
-            case State.PLAYER_INITIAL_UNIT_PLACEMENT:
+            case State.INITIAL_UNIT_PLACEMENT:
                 StartCoroutine(InitialUnitPlacement());
                 break;
 
             case State.BEFORE_PLAYER_TURN:
-                BeforePlayerTurn();
+                currentTurnsPlayer.BeforePlayerTurn(this);
                 break;
 
             case State.PLAYER_TURN:
@@ -59,8 +65,8 @@ public class GameManager : MonoBehaviour
                 break;
 
             case State.AFTER_PLAYER_TURN:
-                StopCoroutine(PlayerTurn());
-                AfterPlayerTurn();
+                StopCoroutine(currentTurnsPlayer.PlayerTurn(this));
+                currentTurnsPlayer.AfterPlayerTurn(this);
                 break;
                 
             case State.PLAYER_WON:
@@ -71,7 +77,6 @@ public class GameManager : MonoBehaviour
 
     IEnumerator InitialUnitPlacement()
     {
-        // Roll Dice To Pick Who Picks First
         currentTurnsPlayerIndex = Random.Range(0, players.Count);
         currentTurnsPlayer = players[currentTurnsPlayerIndex];
 
@@ -99,180 +104,21 @@ public class GameManager : MonoBehaviour
         // Remove one unit to account for territory claiming
         unitsPerPlayer--;
 
-        // Choose Territories
-        // TODO: Change to iterate 42 times when map is expanded to full size
-        for (int i = 0; i < 3; i++)
-        {
-            Territory selectedTerritory = null;
-            // Select Territory With Mouse
-            while(selectedTerritory == null)
-            {
-                // If LMB pressed
-                if (Input.GetMouseButtonDown(0))
-                {
-                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-                    if(hit.collider != null)
-                    {
-                        Territory hitTerritory = hit.collider.GetComponent<Territory>();
-                        if (hitTerritory.owner == null) selectedTerritory = hitTerritory;
-                    }
-                }
-                yield return null;
-            }
-            
-            // Claim Territory
-            selectedTerritory.owner = currentTurnsPlayer;
-            selectedTerritory.unitCount = 1;
-            currentTurnsPlayer.ownedTerritories.Add(selectedTerritory);
+        StartCoroutine(currentTurnsPlayer.InitialUnitPlacement(this, unitsPerPlayer));
 
-            NextPlayer();
-        }
-
-        // Distribute Army 
-        for (int i = 0; i < unitsPerPlayer * players.Count; i++)
-        {
-            // Wait for input, if player clicks a territory it owns add 1 unit
-            Territory selectedTerritory = null;
-            // Select Territory With Mouse
-            while(selectedTerritory == null)
-            {
-                // If LMB pressed
-                if (Input.GetMouseButtonDown(0))
-                {
-                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-                    if(hit.collider != null)
-                    {
-                        Territory hitTerritory = hit.collider.GetComponent<Territory>();
-                        if (hitTerritory.owner == currentTurnsPlayer) selectedTerritory = hitTerritory;
-                    }
-                }
-                yield return null;
-            }
-
-            selectedTerritory.unitCount++;
-
-            NextPlayer();
-        }
-
-        ChangeState(State.PLAYER_TURN);
-    }
-
-    void BeforePlayerTurn()
-    {
-        ChangeState(State.PLAYER_TURN);
+        while (!initialUnitPlacementComplete) yield return null;
+        initialUnitPlacementComplete = false;
+        ChangeState(State.BEFORE_PLAYER_TURN);
     }
 
     IEnumerator PlayerTurn()
     {
-        bool hasPossibleMove = true;
 
-        bool hasTerritoryToAttackWith = false;
-        foreach (Territory t in currentTurnsPlayer.ownedTerritories)
-        {
-            if (t.unitCount > 1) hasTerritoryToAttackWith = true;
-        }
-        if (!hasTerritoryToAttackWith) hasPossibleMove = false;
+        StartCoroutine(currentTurnsPlayer.PlayerTurn(this));
 
-        if (!hasPossibleMove) ChangeState(State.AFTER_PLAYER_TURN);
-
-        // Pick what territory to attack with
-        {
-            Territory selectedTerritory = null;
-            // Select Territory With Mouse
-            while (selectedTerritory == null)
-            {
-                // If LMB pressed
-                if (Input.GetMouseButtonDown(0))
-                {
-                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-                    if (hit.collider != null)
-                    {
-                        Territory hitTerritory = hit.collider.GetComponent<Territory>();
-                        // Check if player has enough units to attack
-                        if (hitTerritory.owner == currentTurnsPlayer && hitTerritory.unitCount > 1) selectedTerritory = hitTerritory;
-                    }
-                }
-                yield return null;
-            }
-
-            currentlyAttackingTerritory = selectedTerritory; 
-
-        }
-        {
-            // Pick who to attack
-            Territory selectedTerritory = null;
-            // Select Territory With Mouse
-            while (selectedTerritory == null)
-            {
-                // If LMB pressed
-                if (Input.GetMouseButtonDown(0))
-                {
-                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-                    if (hit.collider != null)
-                    {
-                        Territory hitTerritory = hit.collider.GetComponent<Territory>();
-                        if (hitTerritory.owner != currentTurnsPlayer) selectedTerritory = hitTerritory;
-                    }
-                }
-                yield return null;
-            }
-
-            currentlyDefendingTerritory = selectedTerritory; 
-
-        }
-
-        // Attack territory
-        currentlyDefendingTerritory.unitCount--;
-
-        // Claim territory 
-        if (currentlyDefendingTerritory.unitCount <= 0)
-        {
-            currentlyDefendingTerritory.owner.ownedTerritories.Remove(currentlyDefendingTerritory);
-            currentlyAttackingTerritory.owner.ownedTerritories.Add(currentlyDefendingTerritory);
-
-            currentlyDefendingTerritory.owner = currentlyAttackingTerritory.owner;
-
-            unitMovementUI.Show();
-
-            while(unitAmountToMoveConfirmed == false)
-            {
-                yield return null;
-            }
-
-            unitMovementUI.Hide();
-            unitAmountToMoveConfirmed = false;
-
-            currentlyDefendingTerritory.unitCount = unitMovementUI.unitsToMove;
-            currentlyAttackingTerritory.unitCount -= unitMovementUI.unitsToMove;
-
-            //currentlyDefendingTerritory.unitCount = currentlyAttackingTerritory.unitCount - 1;
-            //currentlyAttackingTerritory.unitCount = 1;
-        }
-
+        while (!playerTurnComplete) yield return null;
+        playerTurnComplete = false;
         ChangeState(State.AFTER_PLAYER_TURN);
-
-        yield return null;
-    }
-
-    void AfterPlayerTurn()
-    {
-        // Remove defeated players
-        for (int i = 0; i < players.Count; i++)
-        {
-            if (players[i].ownedTerritories.Count == 0)
-            {
-                players.RemoveAt(i);
-            }
-        }
-
-        // Check win condition and change player
-        if(players.Count == 1)
-        {
-            ChangeState(State.PLAYER_WON);
-        }
-
-        NextPlayer();
-        ChangeState(State.BEFORE_PLAYER_TURN);
     }
 
     void PlayerWon()
@@ -280,7 +126,7 @@ public class GameManager : MonoBehaviour
         Debug.Log("Player Won!");
     }
 
-    void NextPlayer()
+    public void NextPlayer()
     {
         currentTurnsPlayerIndex++;
         if(currentTurnsPlayerIndex >= players.Count)
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,4 +16,183 @@ public class Player
         color = playerColor;
         isAI = playerIsAI;
     }
+
+    public IEnumerator InitialUnitPlacement(GameManager gm, int unitsPerPlayer)
+    {
+        // Roll Dice To Pick Who Picks First
+
+        // Choose Territories
+        // TODO: Change to iterate 42 times when map is expanded to full size
+        for (int i = 0; i < 3; i++)
+        {
+            Territory selectedTerritory = null;
+            // Select Territory With Mouse
+            while(selectedTerritory == null)
+            {
+                // If LMB pressed
+                if (Input.GetMouseButtonDown(0))
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+                    if(hit.collider != null)
+                    {
+                        Territory hitTerritory = hit.collider.GetComponent<Territory>();
+                        if (hitTerritory.owner == null) selectedTerritory = hitTerritory;
+                    }
+                }
+                yield return null;
+            }
+            
+            // Claim Territory
+            selectedTerritory.owner = gm.currentTurnsPlayer;
+            selectedTerritory.unitCount = 1;
+            gm.currentTurnsPlayer.ownedTerritories.Add(selectedTerritory);
+
+            gm.NextPlayer();
+
+        }
+
+        // Distribute Army 
+        for (int i = 0; i < unitsPerPlayer * gm.players.Count; i++)
+        {
+            // Wait for input, if player clicks a territory it owns add 1 unit
+            Territory selectedTerritory = null;
+            // Select Territory With Mouse
+            while(selectedTerritory == null)
+            {
+                // If LMB pressed
+                if (Input.GetMouseButtonDown(0))
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+                    if(hit.collider != null)
+                    {
+                        Territory hitTerritory = hit.collider.GetComponent<Territory>();
+                        if (hitTerritory.owner == gm.currentTurnsPlayer) selectedTerritory = hitTerritory;
+                    }
+                }
+                yield return null;
+            }
+
+            selectedTerritory.unitCount++;
+
+            gm.NextPlayer();
+        }
+
+        gm.initialUnitPlacementComplete = true;
+    }
+
+    public void BeforePlayerTurn(GameManager gm)
+    {
+        gm.ChangeState(State.PLAYER_TURN);
+    }
+
+    public IEnumerator PlayerTurn(GameManager gm)
+    {
+        bool hasPossibleMove = true;
+
+        bool hasTerritoryToAttackWith = false;
+        foreach (Territory t in gm.currentTurnsPlayer.ownedTerritories)
+        {
+            if (t.unitCount > 1) hasTerritoryToAttackWith = true;
+        }
+        if (!hasTerritoryToAttackWith) hasPossibleMove = false;
+
+        if (!hasPossibleMove) gm.ChangeState(State.AFTER_PLAYER_TURN);
+
+        // Pick what territory to attack with
+        {
+            Territory selectedTerritory = null;
+            // Select Territory With Mouse
+            while (selectedTerritory == null)
+            {
+                // If LMB pressed
+                if (Input.GetMouseButtonDown(0))
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+                    if (hit.collider != null)
+                    {
+                        Territory hitTerritory = hit.collider.GetComponent<Territory>();
+                        // Check if player has enough units to attack
+                        if (hitTerritory.owner == gm.currentTurnsPlayer && hitTerritory.unitCount > 1) selectedTerritory = hitTerritory;
+                    }
+                }
+                yield return null;
+            }
+
+            gm.currentlyAttackingTerritory = selectedTerritory; 
+
+        }
+        {
+            // Pick who to attack
+            Territory selectedTerritory = null;
+            // Select Territory With Mouse
+            while (selectedTerritory == null)
+            {
+                // If LMB pressed
+                if (Input.GetMouseButtonDown(0))
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+                    if (hit.collider != null)
+                    {
+                        Territory hitTerritory = hit.collider.GetComponent<Territory>();
+                        if (hitTerritory.owner != gm.currentTurnsPlayer) selectedTerritory = hitTerritory;
+                    }
+                }
+                yield return null;
+            }
+
+            gm.currentlyDefendingTerritory = selectedTerritory; 
+
+        }
+
+        // Attack territory
+        gm.currentlyDefendingTerritory.unitCount--;
+
+        // Claim territory 
+        if (gm.currentlyDefendingTerritory.unitCount <= 0)
+        {
+            gm.currentlyDefendingTerritory.owner.ownedTerritories.Remove(gm.currentlyDefendingTerritory);
+            gm.currentlyAttackingTerritory.owner.ownedTerritories.Add(gm.currentlyDefendingTerritory);
+
+            gm.currentlyDefendingTerritory.owner = gm.currentlyAttackingTerritory.owner;
+
+            gm.unitMovementUI.Show();
+
+            while(gm.unitAmountToMoveConfirmed == false)
+            {
+                yield return null;
+            }
+
+            gm.unitMovementUI.Hide();
+            gm.unitAmountToMoveConfirmed = false;
+
+            gm.currentlyDefendingTerritory.unitCount = gm.unitMovementUI.unitsToMove;
+            gm.currentlyAttackingTerritory.unitCount -= gm.unitMovementUI.unitsToMove;
+
+            //currentlyDefendingTerritory.unitCount = currentlyAttackingTerritory.unitCount - 1;
+            //currentlyAttackingTerritory.unitCount = 1;
+        }
+
+        gm.playerTurnComplete = true;
+    }
+
+    public void AfterPlayerTurn(GameManager gm)
+    {
+        // Remove defeated players
+        for (int i = 0; i < gm.players.Count; i++)
+        {
+            if (gm.players[i].ownedTerritories.Count == 0)
+            {
+                gm.players.RemoveAt(i);
+            }
+        }
+
+        // Check win condition and change player
+        if(gm.players.Count == 1)
+        {
+            gm.ChangeState(State.PLAYER_WON);
+        }
+
+        gm.NextPlayer();
+        gm.ChangeState(State.BEFORE_PLAYER_TURN);
+    }
 }
